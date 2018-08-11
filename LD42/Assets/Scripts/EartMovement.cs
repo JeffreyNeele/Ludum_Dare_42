dd1e1135791b3458a5e5779061984af20827da52
Tim <timvanbrederode@live.nl>
2018-08-11 22:15:59 +0200

    more score

--- LD42/Assets/Scripts/EartMovement.cs
+++ LD42/Assets/Scripts/EartMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,13 @@ public class EartMovement : MonoBehaviour {
     public Transform target;
     public float speed;
     private bool jumping;
+    private int multikill;
 
 	// Use this for initialization
 	void Start ()
     {
         jumping = false;
+        multikill = 1;
 	}
 	
 	// Update is called once per frame
@@ -27,6 +30,7 @@ public class EartMovement : MonoBehaviour {
             if (Vector3.Distance(Vector3.zero, transform.position) >= Vector3.Distance(Vector3.zero, GameObject.Find("BlackHole").GetComponent<GameHandler>().powerups[0].transform.position))
             {
                 jumping = false;
+                multikill = 1;
             }
         }
         else
@@ -68,7 +72,8 @@ public class EartMovement : MonoBehaviour {
             else
             {
                 Destroy(collision.gameObject);
-                GameObject.Find("BlackHole").GetComponent<GameHandler>().Score += 10;
+                multikill++;
+                GameObject.Find("BlackHole").GetComponent<GameHandler>().Score += 10 * (float)Math.Pow(1.5f, multikill);
             }
         }
     }
